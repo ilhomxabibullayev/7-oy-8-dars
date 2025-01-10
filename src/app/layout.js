073e395f38cae8684d1edb7268b88ff8151aec5e
ilhomxabibullayev <ilhomxabibullayev2004@gmail.com>
2025-01-10 18:50:11 +0500

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,10 @@ import Logo from '../../public/image/logo.svg';
 import Search from '../../public/icon/search.svg';
 import User from '../../public/icon/user.svg';
 import Cart from '../../public/icon/cart.svg';
+import Logo2 from '../../public/icon/logo2.svg';
+import Instagram from '../../public/icon/instagram.svg';
+import Facebook from '../../public/icon/facebook.svg';
+import Youtube from '../../public/icon/youtube.svg';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -47,13 +51,84 @@ export default function RootLayout({ children }) {
               </li>
             </ul>
             <div className="nav__icon">
-              <Link href='/cart'><Image src={Search}></Image></Link>
-              <Link href='/cart'><Image src={User}></Image></Link>
+              <Image src={Search}></Image>
+              <Link href='/user'><Image src={User}></Image></Link>
               <Link href='/cart'><Image src={Cart}></Image></Link>
             </div>
           </div>
         </nav>
         {children}
+        <footer className="footer">
+          <div className="container footer__container">
+            <div className="footer__block">
+              <div className="footer__content">
+                <Image src={Logo2}></Image>
+                <p className="footer__text">More than just a game. It’s a lifestyle.</p>
+                <div className="footer__image">
+                  <Link href=''><Image src={Instagram}></Image></Link>
+                  <Link href=''><Image src={Facebook}></Image></Link>
+                  <Link href=''><Image src={Youtube}></Image></Link>
+                </div>
+              </div>
+              <div className="footer__content2">
+                <ul className="footer__list">
+                  <h3 className="footer__sub-title">Page</h3>
+                  <li className="footer__item">
+                    <Link className="footer__link" href=''>Home</Link>
+                  </li>
+                  <li className="footer__item">
+                    <Link className="footer__link" href=''>Shop</Link>
+                  </li>
+                  <li className="footer__item">
+                    <Link className="footer__link" href=''>Product</Link>
+                  </li>
+                  <li className="footer__item">
+                    <Link className="footer__link" href=''>Articles</Link>
+                  </li>
+                  <li className="footer__item">
+                    <Link className="footer__link" href=''>Contact Us</Link>
+                  </li>
+                </ul>
+                <ul className="footer__list">
+                  <h3 className="footer__sub-title">Info</h3>
+                  <li className="footer__item">
+                    <Link className="footer__link" href=''>Shipping Policy</Link>
+                  </li>
+                  <li className="footer__item">
+                    <Link className="footer__link" href=''>Return & Refund</Link>
+                  </li>
+                  <li className="footer__item">
+                    <Link className="footer__link" href=''>Support</Link>
+                  </li>
+                  <li className="footer__item">
+                    <Link className="footer__link" href=''>FAQs</Link>
+                  </li>
+                </ul>
+                <ul className="footer__list">
+                  <h3 className="footer__sub-title">Office</h3>
+                  <li className="footer__item2">
+                    <Link className="footer__link2" href=''>43111 Hai Trieu street,</Link>
+                  </li>
+                  <li className="footer__item2">
+                    <Link className="footer__link2" href=''>District 1, HCMC</Link>
+                  </li>
+                  <li className="footer__item2">
+                    <Link className="footer__link2" href=''>Vietnam</Link>
+                  </li>
+                  <Link className="footer__number" href=''>84-756-3237</Link>
+                </ul>
+              </div>
+            </div>
+            <span className="footer__span"></span>
+            <div className="footer__block2">
+              <h2 className="footer__title">Copyright © 2023 3legant. All rights reserved</h2>
+              <div className="footer__block3">
+                <h3 className="footer__sub2-title">Privacy Policy</h3>
+                <h3 className="footer__sub2-title">Terms & Conditions</h3>
+              </div>
+            </div>
+          </div>
+        </footer>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,34 @@
 import Image from "next/image";
+import Link from "next/link";
 import './style.css';
 import Element from '../../public/icon/element.svg';
 import Elements from '../../public/icon/elements.svg';
-import Shop from '../../public/image/shop.png'
-import Shop2 from '../../public/image/shop2.png'
-import Shop3 from '../../public/image/shop3.png'
-import Shop4 from '../../public/image/shop4.png'
-import Shop5 from '../../public/image/shop5.png'
-import Shop6 from '../../public/image/shop6.png'
+import Shop from '../../public/image/shop.png';
+import Shop2 from '../../public/image/shop2.png';
+import Shop3 from '../../public/image/shop3.png';
+import Shop4 from '../../public/image/shop4.png';
+import Shop5 from '../../public/image/shop5.png';
+import Shop6 from '../../public/image/shop6.png';
 import Card from "@/components/Catd";
+import Arrow from '../../public/icon/arrow.svg';
+import Latest from '../../public/image/latest.png'
+import Latest2 from '../../public/image/latest2.png';
+import Latest3 from '../../public/image/latest3.png';
+import Mesaage from '../../public/icon/message.svg';
+import Newsfeed from '../../public/image/newsfeed.png';
+import Newsfeed2 from '../../public/image/newsfeed2.png';
+import Newsfeed3 from '../../public/image/newsfeed3.png';
+import Newsfeed4 from '../../public/image/newsfeed4.png';
+import Newsfeed5 from '../../public/image/newsfeed5.png';
+import Newsfeed6 from '../../public/image/newsfeed6.png';
 
 export default async function Home() {
 
   const PRODUCTS_URL = 'http://localhost:5000/products';
-  
+
   const products = (await (await fetch(PRODUCTS_URL)).json()).data || [];
   console.log(products);
-  
+
   return (
     <>
       <section className="hero">
@@ -40,7 +52,7 @@ export default async function Home() {
             </div>
           </div>
           <div className="product__card">
-          {products.map((product) => (<Card key={product.id} product={product} />))}
+            {products.map((product) => (<Card key={product.id} product={product} />))}
           </div>
         </div>
       </section>
@@ -76,6 +88,93 @@ export default async function Home() {
               <h3 className="shop__sub-title">Accessories</h3>
             </div>
           </div>
+        </div>
+      </section>
+
+      <section className="collection">
+        <div className="container collection__container">
+          <h2 className="collection__title">Shop Collection</h2>
+          <div className="collection__content">
+            <div className="collection__card">
+              <h3 className="collection__sub-title">Juniors Set</h3>
+              <Link className="collection__link" href=''>Collections <Image className="collection__icon" src={Arrow}></Image></Link>
+              <span className="collection__span"></span>
+            </div>
+            <div className="collection__block">
+              <div className="collection__card2">
+                <h3 className="collection__sub2-title">Men’s Set</h3>
+                <Link className="collection__link2" href=''>Collections <Image className="collection__icon" src={Arrow}></Image></Link>
+                <span className="collection__span2"></span>
+              </div>
+              <div className="collection__card3">
+                <h3 className="collection__sub3-title">Women’s Set</h3>
+                <Link className="collection__link3" href=''>Collections <Image className="collection__icon" src={Arrow}></Image></Link>
+                <span className="collection__span3"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="latest">
+        <div className="container latest__container">
+          <div className="latest__content">
+            <h2 className="latest__title">Latest Articles</h2>
+            <div className="latest__block">
+              <Link className="latest__link" href=''>View More <Image className="latest__icon" src={Arrow}></Image></Link>
+              <span className="latest__span"></span>
+            </div>
+          </div>
+          <div className="latest__content2">
+            <div className="latest__card">
+              <Image className="latest__image" src={Latest}></Image>
+              <h3 className="latest__sub-title">Air Jordan x Travis Scott Event</h3>
+              <Link className="latest__link" href=''>Read More <Image className="latest__icon" src={Arrow}></Image></Link>
+              <span className="latest__span"></span>
+            </div>
+            <div className="latest__card">
+              <Image className="latest__image" src={Latest2}></Image>
+              <h3 className="latest__sub-title">Air The timeless classics on the green</h3>
+              <Link className="latest__link" href=''>Read More <Image className="latest__icon" src={Arrow}></Image></Link>
+              <span className="latest__span"></span>
+            </div>
+            <div className="latest__card">
+              <Image className="latest__image" src={Latest3}></Image>
+              <h3 className="latest__sub-title">The 2023 Ryder Cup</h3>
+              <Link className="latest__link" href=''>Read More <Image className="latest__icon" src={Arrow}></Image></Link>
+              <span className="latest__span"></span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="our">
+        <div className="container our__container">
+          <h2 className="our__title">Join Our Newsletter</h2>
+          <h3 className="our__sub-title">Sign up for deals, new products and promotions</h3>
+          <div className="our__form">
+            <Image className="our__image" src={Mesaage}></Image>
+            <input className="our__input" type="text" placeholder="Email address"></input>
+            <p className="our__signup">Signup</p>
+          </div>
+          <span className="our__span"></span>
+        </div>
+      </section>
+
+      <section className="newsfeed">
+        <div className="container newsfeed__container">
+          <h3 className="newsfeed__sub-title">newsfeed</h3>
+          <h2 className="newsfeed__title">Instagram</h2>
+          <p className="newsfeed__text">Follow us on social media for more discount & promotions</p>
+          <Link className="newsfeed__link" href=''>@3legant_official</Link>
+        </div>
+        <div className="newsfeed__block">
+          <Image className="newsfeed__image" src={Newsfeed}></Image>
+          <Image className="newsfeed__image" src={Newsfeed2}></Image>
+          <Image className="newsfeed__image" src={Newsfeed3}></Image>
+          <Image className="newsfeed__image" src={Newsfeed4}></Image>
+          <Image className="newsfeed__image" src={Newsfeed5}></Image>
+          <Image className="newsfeed__image" src={Newsfeed6}></Image>
         </div>
       </section>
     </>
